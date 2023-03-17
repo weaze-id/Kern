c420dcc959dd6a1e04049c27e66bb9a328eb3f8e
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-03-17 18:44:49 +0800

    feat: change authorization service collection extension

--- Authorization/Extensions/ServiceCollectionExtensions.cs
+++ Authorization/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
@@ -11,15 +10,14 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddAuthorization(
         this IServiceCollection services,
-        IConfiguration configuration,
         Action<CookieAuthenticationOptions>? cookieOptions = null,
-        Action<JwtBearerOptions>? jwtOptions = null,
+        Action<JwtBearerOptions, SecurityKey>? jwtOptions = null,
         Action<AuthorizationOptions>? authorizationOptions = null)
     {
         services.AddHttpContextAccessor();
-        services.AddScoped<AuthenticationService>();
 
-        var multiSchemePolicy = new AuthorizationPolicyBuilder(CookieAuthenticationDefaults.AuthenticationScheme,
+        var multiSchemePolicy = new AuthorizationPolicyBuilder(
+                CookieAuthenticationDefaults.AuthenticationScheme,
                 JwtBearerDefaults.AuthenticationScheme)
             .RequireAuthenticatedUser()
             .Build();
@@ -37,23 +35,7 @@ public static class ServiceCollectionExtensions
 
         services
             .AddOptions<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme)
-            .Configure<SecurityKey>(
-                (options, securityKey) =>
-                {
-                    jwtOptions?.Invoke(options);
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        IssuerSigningKey = securityKey,
-                        ValidAudience = configuration["Jwt:Audience"],
-                        ValidIssuer = configuration["Jwt:Issuer"],
-                        RequireSignedTokens = true,
-                        RequireExpirationTime = true,
-                        ValidateLifetime = true,
-                        ValidateAudience = true,
-                        ValidateIssuer = true,
-                        ClockSkew = TimeSpan.Zero
-                    };
-                });
+            .Configure<SecurityKey>((options, securityKey) => jwtOptions?.Invoke(options, securityKey));
 
         return services;
     }
