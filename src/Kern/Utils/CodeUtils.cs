37e295d583acc107d67b64da59c6f5a290e6a7d0
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-06-13 01:50:49 +0700

    feat(kern): add more methods to code utils

--- src/Kern/Utils/CodeUtils.cs
+++ src/Kern/Utils/CodeUtils.cs
@@ -2,10 +2,34 @@ namespace Kern.Utils;
 
 public static class CodeUtils
 {
-    public static string GenerateCode()
+    private static Random random = new Random();
+    private static string numberChars = "0123456789";
+    private static string alphabetChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static string aplhabetAndNumberChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static string GenerateRandomNumberCode(int codeLength)
     {
-        var random = new Random();
-        var randomNumber = random.Next(10000000, 100000000);
-        return randomNumber.ToString();
+        return GenerateRandomCode(numberChars, codeLength);
+    }
+
+    public static string GenerateRandomStringCode(int codeLength)
+    {
+        return GenerateRandomCode(alphabetChars, codeLength);
+    }
+
+    public static string GenerateRandomCode(int codeLength)
+    {
+        return GenerateRandomCode(aplhabetAndNumberChars, codeLength);
+    }
+
+    private static string GenerateRandomCode(string chars, int codeLength)
+    {
+        Span<char> code = new char[codeLength];
+        for (int i = 0; i < codeLength; i++)
+        {
+            code[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(code);
     }
 }
